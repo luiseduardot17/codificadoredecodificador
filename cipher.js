731ab88bc9f6f13ef105e54069b77c8ea8004822
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-05-05 18:08:09 -0300

    cifras funcionais e mudanças no codigo

--- cipher.js
+++ cipher.js
@@ -1,3 +1,114 @@
+// BOTÕES DE SELECAO: aparecer deslocamento
+var selecao = document.querySelector('#cifras');
+var incremento = document.querySelector('#incremento');
+incremento.style.visibility = 'hidden';
+
+selecao.addEventListener('click', function (e) {
+  if (selecao.value == "1") {
+    incremento.style.visibility = 'visible';
+    incremento.value = '';
+  } else {
+    incremento.style.visibility = 'hidden';
+  }
+});
+
+// SELECIONAR OS RADIO BUTTONS CRIA UM BOTAO
+var radiais = document.querySelector('#radiais');
+var encode = document.querySelector('#codificar');
+var decode = document.querySelector('#decodificar');
+
+radiais.addEventListener('click', function (event) {
+  if (encode.checked) {
+    btnGo.innerHTML = `<input class="btn" id="btnCodificar" type="button" value="Codificar mensagem!">`
+    document.querySelector('#btnCodificar').addEventListener('click', codificar);
+  } else if (decode.checked) {
+    btnGo.innerHTML = `<input class="btn" id="btnDecodificar" type="button" value="Decodificar mensagem!">`
+    document.querySelector('#btnDecodificar').addEventListener('click', decodificar);
+  }
+})
+
+//CODIFICAR BASE64 E CIFRA DE CESAR
+var result = document.querySelector('#resultado')
+
+function codificar() {
+  var entradaTxT = document.querySelector('#mensagem').value
+  if (selecao.value == '1' && encode.checked) {
+    codificaCesar()
+  } else if (selecao.value == '2' && encode.checked) {
+    var codificarStr = entradaTxT
+    result.value = btoa(codificarStr);
+  }
+};
+
+//DECODIFICAR BASE64 E CIFRA DE CESAR
+function decodificar() {
+  var entradaTxT = document.querySelector('#mensagem').value
+  if (selecao.value == '1' && decode.checked) {
+    decodificaCesar()
+  } else if (selecao.value == '2' && decode.checked) {
+    var decodificarStr = atob(entradaTxT);
+    result.innerHTML = decodificarStr;
+  }
+};
+
+function codificaCesar() {
+  var entradaTxT = document.querySelector('#mensagem').value
+  var deslocamento = document.querySelector('#deslocamento').value
+  var msgValue = ""
+  // CODIFICAR EM CIFRA DE CESAR /PARA LETRAS MAIUSCULAS E MINUSCULAS
+  for (var i = 0; i < entradaTxT.length; i++) {
+    var passos = parseInt(deslocamento)
+    var asciiNumber = entradaTxT[i].charCodeAt()
+    if (asciiNumber >= 65 && asciiNumber <= 90) {
+      var passoTotal = asciiNumber + passos
+      if (passoTotal > 90) {
+        passoTotal = passoTotal - 26
+      }
+      msgValue += String.fromCharCode(passoTotal)
+    } else if (asciiNumber >= 97 && asciiNumber <= 122) {
+      var passoTotal = asciiNumber + passos
+      if (passoTotal > 122) {
+        passoTotal = passoTotal - 26
+      }
+      msgValue += String.fromCharCode(passoTotal)
+    } else {
+      msgValue += entradaTxT[i]
+    }
+  }
+  result.value = msgValue
+}
+function decodificaCesar() {
+  var entradaTxT = document.querySelector('#mensagem').value
+  var deslocamento = document.querySelector('#deslocamento').value
+  var msgValue = ""
+  // DECODIFICAR EM CIFRA DE CESAR /PARA LETRAS MAIUSCULAS E MINUSCULAS
+  for (var i = 0; i < entradaTxT.length; i++) {
+    var passos = parseInt(deslocamento)
+    var asciiNumber = entradaTxT[i].charCodeAt()
+    if (asciiNumber >= 65 && asciiNumber <= 90) {
+      var passoTotal = asciiNumber - passos
+      if (passoTotal < 65) {
+        passoTotal = passoTotal + 26
+      }
+      msgValue += String.fromCharCode(passoTotal)
+    } else if (asciiNumber >= 97 && asciiNumber <= 122) {
+      var passoTotal = asciiNumber - passos
+      if (passoTotal < 97) {
+        passoTotal = passoTotal + 26
+      }
+      msgValue += String.fromCharCode(passoTotal)
+    } else {
+      msgValue += entradaTxT[i]
+    }
+  }
+  result.value = msgValue
+}
+
+//BOTÃO DE RESET
+function reload() {
+  location.reload();
+};
+
 /*Cifra de César
  A B C D E F G H I J K  L  M
  0 1 2 3 4 5 6 7 8 9 10 11 12
@@ -5,41 +116,6 @@
  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
  13 14 15 16 17 18 19 20 21 22 23 24 25
 
- (codigoDaLetra + 7) % 26             */
+ ASCII maiusculas: 65 a 90; minusculas: 97 a 122
 
-// PRECISA ARRUMAR OS BOTÕES DE SELECAO
-var selecao = document.querySelector('#cifras');
-var incrementoUm = document.querySelector('#incrementoUm');
-incrementoUm.style.visibility = '';
-
-selecao.addEventListener("onchange", function() {
-  if (selecao.cifras.value) {
-    incrementoUm.style.visibility = 'visible';
-    incrementoUm.value = '';
-  } else {
-    incrementoUm.style.visibility = 'hidden';
-  }
-});
-
-// Base64
-//recebe a mensagem para codificar em BASE64
-var btnCodificar = document.querySelector('#btnCodificarBase64');
-var btnDecodificar = document.querySelector('#btnDecodificarBase64');
-var result = document.querySelector('#resultado');
-
-btnCodificar.addEventListener("click", function (e) {
-  e.preventDefault()
-  var recebeMensagemC = document.querySelector('#mensagem').value;
-  var codificarStr = btoa(recebeMensagemC);
-  result.innerHTML = codificarStr;
-})
-btnDecodificar.addEventListener("click", function (e) {
-  e.preventDefault();
-  var recebeMensagemD = document.querySelector('#mensagem').value;
-  var decodificarStr = atob(recebeMensagemD);
-  result.innerHTML = decodificarStr;
-});
-
-function reload() {
-  location.reload();
-}
+(codigoDaLetra + numdeslocamento) % 26 */
